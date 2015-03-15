27dd2af3f2e1021abe7b11d1dfcc410011ed187f
samier958 <samier958@gmail.com>
2015-03-15 23:29:06 +0800

    Add heater's history.

--- cheaterhistoryrecord.cpp
+++ cheaterhistoryrecord.cpp
@@ -5,6 +5,9 @@
 
 #include "cheaterclient.h"
 
+#include <QDate>
+#include <QDateTime>
+
 CHeaterHistoryRecord::CHeaterHistoryRecord(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::CHeaterHistoryRecord)
@@ -20,7 +23,79 @@ void CHeaterHistoryRecord::setModbusMaster(QModbusMaster *modbusMaster)
 {
     pModbusMaster = modbusMaster;
 }
+
+
+
+void CHeaterHistoryRecord::updateHeaterHistoryRecord()
+{
+    //work record
+    QModbusRegisters workRecordRegisters(HEATER_WORK_RECORD_BASE, HEATER_WORK_RECORD_LENGTH);
+    pModbusMaster->connect();
+    pModbusMaster->readInputRegisters(workRecordRegisters);
+    pModbusMaster->close();
+    //fault record
+    QModbusRegisters faultRecordRegisters(HEATER_FAULT_RECORD_BASE, HEATER_FAULT_RECORD_LENGTH);
+    pModbusMaster->connect();
+    pModbusMaster->readInputRegisters(faultRecordRegisters);
+    pModbusMaster->close();
+
+    for(int i = 0; i  < HEATER_WORK_OR_FAULT_HISTORY_RECORD_TIEM_NUM; i ++){
+        m_workRecord[i] = QString::number((i + 1));
+        m_faultRecord[i] = QString::number((i + 1));
+        for(int j = 0; j < HEATER_WORK_OR_FAULT_HISTORY_RECORD_TIEM_LENGTH; j ++){
+            m_workRecord[i] +=QString::number(( workRecordRegisters.getInteger16(i * HEATER_WORK_OR_FAULT_HISTORY_RECORD_TIEM_LENGTH + j)))
+                    + "--";
+            m_faultRecord[i] +=QString::number(( faultRecordRegisters.getInteger16(i * HEATER_WORK_OR_FAULT_HISTORY_RECORD_TIEM_LENGTH + j)))
+                    + "--";
+        }
+    }
+
+
+}
+void CHeaterHistoryRecord::showHeaterHistoryRecord()
+{
+    ui->workRecord->clear();
+    ui->workRecord->clear();
+    for(int i = 0; i < HEATER_WORK_OR_FAULT_HISTORY_RECORD_TIEM_NUM; i ++){
+        ui->workRecord->addItem(m_workRecord[i]);
+        ui->faultRecord->addItem(m_faultRecord[i]);
+    };
+}
+
+
 void CHeaterHistoryRecord::reflashHeaterControlForm(int index)
 {
+    if(CHeaterClient::HistoryRecord == index){updateHeaterHistoryRecord();}
+}
 
+void CHeaterHistoryRecord::on_read_released()
+{
+    QModbusRegisters recordTimeRegisters(HEATER_HISTORY_RECORD_BASE, HEATER_HISTORY_RECORD_ITEM_LENGTH);
+    int time[HEATER_HISTORY_RECORD_TIME_ITEM];
+    bool ok;
+    time[0] = ui->recordTimeYear->text().toInt(&ok);
+    time[1] = ui->recordTimeMonth->text().toInt(&ok);
+    time[2] = ui->recordTimeDay->text().toInt(&ok);
+    time[3] = ui->recordTimeHour->text().toInt(&ok);
+    time[4] = ui->recordTimeMinute->text().toInt(&ok);
+    time[4] = time[4] - (time[4] % 5);
+    for(int i = 0; i < HEATER_HISTORY_RECORD_TIME_ITEM; i ++){
+        recordTimeRegisters.setInteger16(i, time[i]);
+    }
+    pModbusMaster->connect();
+    pModbusMaster->writeRegisters(recordTimeRegisters);
+    pModbusMaster->close();
+
+
+}
+
+void CHeaterHistoryRecord::on_cancle_released()
+{
+    QDate date = QDate::currentDate();
+    QTime time = QTime::currentTime();
+    ui->recordTimeYear->setText(QString::number(date.year()));
+    ui->recordTimeMonth->setText(QString::number(date.month()));
+    ui->recordTimeDay->setText(QString::number(date.day()));
+    ui->recordTimeHour->setText(QString::number(time.hour()));
+    ui->recordTimeMinute->setText(QString::number(time.minute()));
 }
--- cheaterhistoryrecord.h
+++ cheaterhistoryrecord.h
@@ -5,11 +5,17 @@
 
 #include "qmodbusmaster.h"
 
+
+#define HEATER_WORK_OR_FAULT_HISTORY_RECORD_TIEM_NUM 20
+#define HEATER_WORK_OR_FAULT_HISTORY_RECORD_TIEM_LENGTH 6
+
 using namespace Modbus;
 
 namespace Ui {
 class CHeaterHistoryRecord;
 }
+
+
 
 class CHeaterHistoryRecord : public QWidget
 {
@@ -21,8 +27,17 @@ public:
 
     void setModbusMaster(QModbusMaster *modbusMaster);
 
+
+    void updateHeaterHistoryRecord();
+    void showHeaterHistoryRecord();
+
 public slots:
     void reflashHeaterControlForm(int index);
+
+private slots:
+    void on_read_released();
+
+    void on_cancle_released();
 
 private:
     Ui::CHeaterHistoryRecord *ui;
@@ -31,6 +46,9 @@ private:
     QModbusMaster *pModbusMaster;
 
     int m_time[5];
+
+    QString m_workRecord[HEATER_WORK_OR_FAULT_HISTORY_RECORD_TIEM_NUM];
+    QString m_faultRecord[HEATER_WORK_OR_FAULT_HISTORY_RECORD_TIEM_NUM];
 };
 
 #endif // CHEATERHISTORYRECORD_H
